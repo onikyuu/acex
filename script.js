4403426d4ab4e80c527f14f03b89d8b4f6ca6424
onikyuu <128197230+onikyuu@users.noreply.github.com>
2024-06-08 03:26:23 -0300

    script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const feedbackButton = document.getElementById("feedbackButton");
+    const feedbackForm = document.getElementById("feedbackForm");
+    const closeButton = document.getElementById("closeButton");
+
+    feedbackButton.addEventListener("click", function() {
+        feedbackForm.style.display = "block";
+    });
+
+    closeButton.addEventListener("click", function() {
+        feedbackForm.style.display = "none";
+    });
+});
+    
